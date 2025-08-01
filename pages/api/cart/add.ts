df5141fa3c13c622ebf0ec3853893b555a47c209
yassin549 <khoualdiyassin26@gmail.com>
2025-08-01 09:59:45 +0100

    commit

--- pages/api/cart/add.ts
+++ pages/api/cart/add.ts
@@ -1,25 +1,54 @@
 import { getIronSession } from 'iron-session'
 import { NextApiRequest, NextApiResponse } from 'next'
 import { sessionOptions } from '@/lib/session'
-import { CartItem } from '@/types'
+import { CartItem, Product } from '@/types'
 
 export default async function addToCartRoute(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req.method === 'POST') {
-    const session = await getIronSession(req, res, sessionOptions)
-    const { product, quantity, selectedColor, selectedSize } = req.body
+  if (req.method !== 'POST') {
+    res.setHeader('Allow', ['POST'])
+    return res.status(405).end(`Method ${req.method} Not Allowed`)
+  }
 
+  try {
+    const {
+      product,
+      quantity,
+      selectedColor,
+      selectedSize,
+    }: {
+      product: Product
+      quantity: number
+      selectedColor: string
+      selectedSize: string
+    } = req.body
+
+    // Basic validation
+    if (!product || !quantity || !selectedColor || !selectedSize) {
+      return res.status(400).json({ message: 'Missing required fields.' })
+    }
+
+    if (typeof quantity !== 'number' || quantity <= 0) {
+      return res.status(400).json({ message: 'Invalid quantity.' })
+    }
+
+    const session = await getIronSession(req, res, sessionOptions)
     const cart = session.cart || { items: [], total: 0 }
 
     const existingItemIndex = cart.items.findIndex(
-      (item: CartItem) => item.id === product.id
+      (item: CartItem) =>
+        item.id === product.id &&
+        item.selectedColor === selectedColor &&
+        item.selectedSize === selectedSize
     )
 
     if (existingItemIndex > -1) {
+      // Item exists, update quantity
       cart.items[existingItemIndex].quantity += quantity
     } else {
+      // Item does not exist, add as new
       const newCartItem: CartItem = {
         id: product.id,
         name: product.name,
@@ -32,6 +61,7 @@ export default async function addToCartRoute(
       cart.items.push(newCartItem)
     }
 
+    // Recalculate total
     cart.total = cart.items.reduce(
       (acc: number, item: CartItem) => acc + item.price * item.quantity,
       0
@@ -40,9 +70,11 @@ export default async function addToCartRoute(
     session.cart = cart
     await session.save()
 
-    res.status(200).json(cart)
-  } else {
-    res.setHeader('Allow', ['POST'])
-    res.status(405).end(`Method ${req.method} Not Allowed`)
+    return res.status(200).json(cart)
+  } catch (error) {
+    console.error(error)
+    return res
+      .status(500)
+      .json({ message: 'An internal server error occurred.' })
   }
 }
